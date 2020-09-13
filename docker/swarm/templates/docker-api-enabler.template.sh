a5e7e68690d81184d8faac187e1f8e19a0dd69ca
Zsombor Hunyadi <zsombor.hunyadi@gmail.com>
2020-09-13 22:53:15 +0200

    Jenkins docker init.

---
+++ docker/swarm/templates/docker-api-enabler.template.sh
@@ -0,0 +1,4 @@
+sudo mkdir /etc/systemd/system/docker.service.d
+sudo cp ./startup_options.conf /etc/systemd/system/docker.service.d/
+sudo systemctl daemon-reload
+sudo systemctl restart docker.service

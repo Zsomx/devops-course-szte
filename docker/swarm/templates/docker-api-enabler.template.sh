f0e649a2837d7ff2d9ba25b491976b4e7dc427ef
Zsombor Hunyadi <zsombor.hunyadi@gmail.com>
2020-09-19 17:11:36 +0200

    Swarm infrastructure update: registry enabled, remote Docker api enabled

--- docker/swarm/templates/docker-api-enabler.template.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir /etc/systemd/system/docker.service.d
-sudo cp ./startup_options.conf /etc/systemd/system/docker.service.d/
-sudo systemctl daemon-reload
-sudo systemctl restart docker.service

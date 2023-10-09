0de023981bee467438feef10d38588baf4324903
Zsombor Hunyadi <zsombor.hunyadi@gmail.com>
2023-10-09 13:15:16 +0200

    Fix bootstrap.sh with npm

--- vagrant/network/bootstrap.sh
+++ vagrant/network/bootstrap.sh
@@ -3,4 +3,4 @@ apt update
 apt install -y apt-transport-https ca-certificates curl software-properties-common git
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
-apt install -y nodejs build-essential
+apt install -y nodejs build-essential npm
